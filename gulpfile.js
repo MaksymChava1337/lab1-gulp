11488f11c402610b2417070b1b1ef23562bb343f
MaksymChava1337 <maksymchava@gmail.com>
2025-10-20 07:03:38 +0300

    зробив браузер сінк

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
 
 
 //css task
@@ -11,9 +12,41 @@ function cssTask(done){
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename('style.css'))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(browserSync.stream());
     done();
 }
 
 
-gulp.task(cssTask);
+
+function syncTask(done){
+
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        },
+        port: 3000
+    });
+
+    done();
+}
+
+function browserReload(done){
+    browserSync.reload();
+    done();
+}
+
+function watchFiles(){
+    gulp.watch("./app/scss/**/*", cssTask);
+     gulp.watch("./**/*.html", browserReload);
+      gulp.watch("./**/*.js", browserReload);
+}
+
+
+// function watchSass(){
+//     gulp.watch("./app/**/*", cssTask);
+// }
+// gulp.task(print);
+// gulp.task(cssTask);
+
+gulp.task('default', gulp.parallel(syncTask, watchFiles));
