96f573078023ac6e10c4306b5b09d8d40cc90a94
MaksymChava1337 <maksymchava@gmail.com>
2025-10-20 04:40:02 +0300

    зробив css таску

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
-function defaultSomeTask(done){
-    console.log("All is working!");
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+
+
+//css task
+function cssTask(done){
+    gulp.src('./app/scss/style.scss')
+    .pipe(sass())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename('style.css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./dist/css/'));
     done();
 }
 
-exports.default = defaultSomeTask;
+
+gulp.task(cssTask);
